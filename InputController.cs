e3cf0171b523c16664b78d3edf4e49ead1aa4747
zxcv9675 <zxcv9675@naver.com>
2023-11-28 20:19:19 +0900

    cs file revise

--- InputController.cs
+++ InputController.cs
@@ -7,9 +7,17 @@ public class InputController : MonoBehaviour
     [SerializeField]
     private Camera     mainCamera;
     private TileType   currentType = TileType.Empty;   // 마우스 클릭된 위치의 타일을 currentType 속성으로 변경
+    private Tile        playerTile = null;   // 플레이어 타일 정보
 
+    [SerializeField]
+    private CameraController    cameraController;  // 카메라 위치, 줌 제어를 위한 CameraController
+    private Vector2    previousMousePosition;  // 직전 프레임의 마우스 위치
+    private Vector2    currentMousePosition;  // 현재 프레임의 마우스 위치
     private void Update()
     {
+        // 카메라 이동, Zoom In/Out
+        UpdateCamera();
+        
         RaycastHit hit;
         
         // 마우스 왼쪽 버튼을 누르고 있을 때
@@ -25,6 +33,19 @@ public class InputController : MonoBehaviour
                 Tile tile = hit.transform.GetComponent<Tile>();
                 if (tile != null)
                 {
+                    // 플에이어 타일은 맵에 1개만 배치할 수 있기 때문에
+                    // 이전에 배치된 플레이어 타일이 없으면 Empty 속성으로 설정
+                    if ( currentType == TileType.Player )
+                    {
+                        if ( playerTile != null)
+                        {
+                            playerTile.TileType = TileType.Empty;
+                        }
+                        playerTile = tile;
+                    }
+                 
+                    
+                    
                     // 부딪힌 오브젝트를 tileType 속성으로 변경 (타일, 아이템, 플레이어 캐릭터)
                     tile.TileType = currentType;
                 }
@@ -42,6 +63,33 @@ public class InputController : MonoBehaviour
         currentType = (TileType)tileType;
     }
 
+    public void UpdateCamera()
+    {
+        // 키보드를 이용한 카메라 이동
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        cameraController.SetPosition(x,y);
+
+        // 마우스 휠 버튼을 이용한 카메라 이동
+        if ( Input.GetMouseButtonDown(2) )
+        {
+            currentMousePosition = previousMousePosition = Input.mousePosition;
+        }
+        else if ( Input.GetMouseButton(2) )
+        {
+            currentMousePosition = Input.mousePosition;
+            if ( previousMousePosition != currentMousePosition )
+            {
+                Vector2 move = (previousMousePosition-currentMousePosition) * 0.5f;
+                cameraController.SetPosition(move.x,move.y);
+            }
+        }
+        previousMousePosition = currentMousePosition;
+
+        // 마우스 휠을 이용한 카메라 Zoom In/Out
+        float distance = Input.GetAxisRaw("Mouse ScrollWheel");
+        cameraController.SetOrthographicSize(-distance);
+    }
     void Start()
     {
         
