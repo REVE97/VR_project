1907060353e072b3aabb6274cbfef8a774458b26
zxcv9675 <zxcv9675@naver.com>
2023-12-15 16:04:11 +0900

    Add revise

--- PlayerMove.cs
+++ PlayerMove.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float maxSpeed;
     Rigidbody2D rigid;
 
     void Awake()
@@ -11,10 +12,25 @@ public class PlayerMove : MonoBehaviour
         rigid = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        // Stop Speed 
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.normalized.x * 0.5f, rigid.velocity.y);
+        }
+    }
+
     void FixedUpdate()
     {
+        // Move speed
         float h = Input.GetAxisRaw("Horizontal");
 
         rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+        if (rigid.velocity.x > maxSpeed)   // Right Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+        else if (rigid.velocity.x < maxSpeed*(-1) )   // Left Max Speed
+            rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
     }
 }
